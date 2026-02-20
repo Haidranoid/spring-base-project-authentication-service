24f181767d08bd4202beedb026cb5997a0395800
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-02-20 16:21:03 -0600

    v0.0.14

--- src/test/java/com/springbaseproject/authenticationservice/fixtures/AuthenticationEntityFixtures.java
+++ src/test/java/com/springbaseproject/authenticationservice/fixtures/AuthenticationEntityFixtures.java
@@ -6,17 +6,6 @@ import com.springbaseproject.sharedstarter.entities.TokenEntity;
 
 public class AuthenticationEntityFixtures {
 
-    public static AccountEntity currentSession() {
-        return AccountEntity.builder()
-                .username("admin")
-                .firstName("Steve")
-                .lastName("Rogers")
-                .email("admin@email.com")
-                .password("<PASSWORD>")
-                .role(Roles.ADMIN)
-                .build();
-    }
-
     public static AccountEntity adminAccountPersisted(Long id) {
         return AccountEntity.builder()
                 .id(id)
