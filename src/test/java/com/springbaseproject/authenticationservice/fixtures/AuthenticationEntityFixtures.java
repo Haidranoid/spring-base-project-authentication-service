2fbf2d5751c423f07da55184e15c1f5ff52fa4aa
Eduardo Martinez <martinezlara_joseeduardo@hotmail.com>
2026-02-22 14:53:22 -0600

    v0.0.15

--- src/test/java/com/springbaseproject/authenticationservice/fixtures/AuthenticationEntityFixtures.java
+++ src/test/java/com/springbaseproject/authenticationservice/fixtures/AuthenticationEntityFixtures.java
@@ -1,23 +1,8 @@
 package com.springbaseproject.authenticationservice.fixtures;
 
-import com.springbaseproject.sharedstarter.constants.Roles;
-import com.springbaseproject.sharedstarter.entities.AccountEntity;
 import com.springbaseproject.sharedstarter.entities.TokenEntity;
 
 public class AuthenticationEntityFixtures {
-
-    public static AccountEntity adminAccountPersisted(Long id) {
-        return AccountEntity.builder()
-                .id(id)
-                .username("bugsbunny12")
-                .firstName("Bugs")
-                .lastName("Bunny")
-                .email("bunny@email.com")
-                .password("<password>")
-                .role(Roles.ADMIN)
-                .enabled(true)
-                .build();
-    }
 
     public static TokenEntity accessToken() {
         return TokenEntity.builder()
--- src/test/java/com/springbaseproject/authenticationservice/unit/mappers/AuthMapperTest.java
+++ src/test/java/com/springbaseproject/authenticationservice/unit/mappers/AuthMapperTest.java
@@ -3,11 +3,8 @@ package com.springbaseproject.authenticationservice.unit.mappers;
 import com.springbaseproject.authenticationservice.common.dtos.AuthAccountDto;
 import com.springbaseproject.authenticationservice.common.dtos.AuthResponseDto;
 import com.springbaseproject.authenticationservice.fixtures.AuthenticationDtoFixtures;
-import com.springbaseproject.authenticationservice.fixtures.AuthenticationEntityFixtures;
 import com.springbaseproject.authenticationservice.mappers.AuthMapper;
 import com.springbaseproject.authenticationservice.mappers.impl.AuthMapperImpl;
-import com.springbaseproject.sharedstarter.constants.Roles;
-import com.springbaseproject.sharedstarter.entities.AccountEntity;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,21 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AuthMapperTest {
     
     private final AuthMapper authMapper = new AuthMapperImpl();
-
-    /*
-    @Test
-    void toAuthAccountDto_shouldMapAllFields_fromAccountEntity() {
-        AccountEntity accountEntity = AuthenticationEntityFixtures.adminAccountPersisted(1L);
-        AuthAccountDto authAccountDto = authMapper.toAuthAccountDto(accountEntity);
-
-        assertThat(authAccountDto.id()).isEqualTo(1L);
-        assertThat(authAccountDto.username()).isEqualTo("bugsbunny12");
-        assertThat(authAccountDto.firstName()).isEqualTo("Bugs");
-        assertThat(authAccountDto.lastName()).isEqualTo("Bunny");
-        assertThat(authAccountDto.email()).isEqualTo("bunny@email.com");
-        assertThat(authAccountDto.role()).isEqualTo(Roles.ADMIN);
-    }
-    */
 
     @Test
     void toAuthResponseDto_shouldMapAllFields_fromAuthAccountDto() {
